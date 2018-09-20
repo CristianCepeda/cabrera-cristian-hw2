3a400a6372cc64eedcdf9dbddd4f1a633dd40c16
Marcial <marcialandres@me.com>
2018-09-20 19:54:31 -0400

    trying to fix

--- part1/myshell.c
+++ part1/myshell.c
@@ -10,96 +10,90 @@
 #define MAX_ARGS 20
 #define BUFSIZ 1024
 
-//To separete process by semicolons :)
-int get_process(char* cmdline, char* process[])
+int get_number_of_processes(char* cmdline, char* args[]) {
+	int i = 0;
+
+	/* if no args */
+	if ((args[0] = strtok(cmdline, ";")) == NULL)
+		return 0;
+
+	while ((args[++i] = strtok(NULL, ";")) != NULL) {
+		if (i >= MAX_ARGS) {
+			printf("Too many arguments!\n");
+			exit(1);
+		}
+	}
+	/* the last one is always NULL */
+	return i;
+}
+
+
+int* get_args(char* cmdline, char* args[][], int num_of_procs, char* procs[])
 {
-  int i = 0;
+  int nargs[num_of_procs];
+  for (int j = 0; j < num_of_procs; j++) {
+	  int i = 0;
+	  /* if no args */
+	  if ((args[0][0] = strtok(procs[j], "\n\t ")) == NULL)
+		  return 0;
 
-  /* if no process */
-  if((process[0] = strtok(cmdline, ";")) == NULL)
-    return 0;
+	  while ((args[j][++i] = strtok(NULL, "\n\t ")) != NULL) {
+		  if (i >= MAX_ARGS) {
+			  printf("Too many arguments!\n");
+			  exit(1);
+		  }
+	  }
 
-  while((process[++i] = strtok(NULL, ";")) != NULL) {
-    if(i >= MAX_ARGS) {
-      printf("Too many arguments!\n");
-      exit(1);
-    }
+	  nargs[j] = i;
   }
   /* the last one is always NULL */
-  return i;
+
+  return nargs;
 }
-
-int get_args(char* cmdline, char* args[][MAX_ARGS], char* process[], int num_processes)
-{
-  int j;
-
-  for (int x = 0; x < num_processes; x++){
-  int i = 0;
-  /* if no args */
-  if((args[0][0] = strtok(process[x], "\n\t ")) == NULL)
-    return 0;
-
-  while((args[x][++i] = strtok(NULL, "\n\t ")) != NULL) {
-    if(i >= MAX_ARGS) {
-      printf("Too many arguments!\n");
-      exit(1);
-    }
-  }
-  j = i;
-}
-  /* the last one is always NULL */
-  return j;
-}
-
 
 void execute(char* cmdline)
 {
   int pid, async;
-  char* process[MAX_ARGS];
+  char* procs[MAX_ARGS];
+  int num_of_procs = get_number_of_processes(cmdline, procs);
+  // Multi Dimensional Array of Proccesses
+  char* args[num_of_procs][MAX_ARGS];
 
-  int num_processes = get_process(cmdline, process);
+  if (num_of_procs <= 0) return;
 
-  char* args[num_processes][MAX_ARGS];
+  int* nargs = get_args(cmdline, args, num_of_procs, args_procs);
 
 
 
-  int nargs = get_args(cmdline, args,process, num_processes);
-  if(nargs <= 0) return;
-
-/*
-strcmp() compares two strings
-Returns 0 if they are equal which will make the if statement true
-and thus
-*/
-
-for(int i; i < num_processes; i++){
-
-  char* check_args[MAX_ARGS];
-
-  check_args[i] = args[0][1];
-
-  if(!strcmp(args[0][i], "quit") || !strcmp(args[0][i], "exit")) {
+  if(!strcmp(args[0][0], "quit") || !strcmp(args[0][0], "exit")) {
     exit(0);
   }
 
-  /* check if async call */
-  if(!strcmp(args[nargs-1][i], "&")) { async = 1; args[--nargs][i] = 0; }
-  else async = 0;
 
-  pid = fork();
-  if(pid == 0) { /* child process */
-    execvp(args[0][i], check_args);
-    /* return only when exec fails */
-    perror("exec failed");
-    exit(-1);
-  } else if(pid > 0) { /* parent process */
-    if(!async) waitpid(pid, NULL, 0);
-    else printf("PID %d\n", getpid());
-  } else { /* error occurred */
-    perror("fork failed");
-    exit(1);
+  for (int i = 0; i < num_of_procs; i++) {
+
+	  /* check if async call */
+	  if (!strcmp(args[nargs[i]- 1], "&")) { async = 1; args[--nargs] = 0; }
+	  else async = 0;
+
+	  pid = fork();
+	  if (pid == 0) { /* child process */
+		  execvp(args[0], args);
+		  /* return only when exec fails */
+		  perror("exec failed");
+		  exit(-1);
+	  }
+	  else if (pid > 0) { /* parent process */
+		  if (!async) waitpid(pid, NULL, 0);
+		  else printf("this is an async call\n");
+	  }
+	  else { /* error occurred */
+		  perror("fork failed");
+		  exit(1);
+	  }
+
   }
-}
+
 
 }
 
@@ -108,12 +102,11 @@ int main (int argc, char* argv [])
   char cmdline[BUFSIZ];
 
   for(;;) {
-    printf("Cabrera-Cristian> ");
+    printf("JohnSmith> ");
     if(fgets(cmdline, BUFSIZ, stdin) == NULL) {
       perror("fgets failed");
       exit(1);
     }
-    /*Reads the buffer or the input that was placed by the user*/
     execute(cmdline) ;
   }
   return 0;
