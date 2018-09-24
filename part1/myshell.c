2844dca684bde55ff38da76b0ab8f578796fe679
CristianCepeda <ccepe014@fiu.edu>
2018-09-24 15:41:31 -0400

    add notes on what some of the functions are doing

--- part1/myshell.c
+++ part1/myshell.c
@@ -1,5 +1,5 @@
 /* This example is stolen from Dr. Raju Rangaswami's original 4338
-   demo and modified to fit into our lecture. */
+demo and modified to fit into our lecture. */
 
 #include <stdio.h>
 #include <sys/wait.h>
@@ -10,72 +10,79 @@
 #define MAX_ARGS 20
 #define BUFSIZ 1024
 
-int get_args(char* cmdline, char* args[])
-{
-  int i = 0;
+/* Use of this function
+*************************/
+// This function returns an i which is the number of arguments the user inputs.
+// If the number of input is greater than MAX_ARGS then returb
 
-  /* if no args */
-  if((args[0] = strtok(cmdline, "\n\t ")) == NULL)
-    return 0;
+int get_args(char* cmdline, char* args[]) {
+	int i = 0;
 
-  while((args[++i] = strtok(NULL, "\n\t ")) != NULL) {
-    if(i >= MAX_ARGS) {
-      printf("Too many arguments!\n");
-      exit(1);
-    }
-  }
-  /* the last one is always NULL */
-  return i;
+	/* if no args */
+	/* UNDERSTAND WHAT strtok() delimits the strings */
+	if((args[0] = strtok(cmdline, "\n\t ")) == NULL)
+	return 0;
+
+	while((args[++i] = strtok(NULL, "\n\t ")) != NULL) {
+		if(i >= MAX_ARGS) {
+			printf("Too many arguments!\n");
+			exit(1);
+		}
+	}
+	/* the last one is always NULL */
+	return i;
 }
 
+/* Recives an array of characters and this the commandline input*/
 void execute(char* cmdline)
 {
-  int pid, async;
-  char* args[MAX_ARGS];
+	int pid, async;
+	char* args[MAX_ARGS];
 
-  int nargs = get_args(cmdline, args);
-  if(nargs <= 0) return;
+	int nargs = get_args(cmdline, args);
+	if(nargs <= 0) return;
 
-/*
-strcmp() compares two strings
-Returns 0 if they are equal which will make the if statement true
-and thus
-*/
-  if(!strcmp(args[0], "quit") || !strcmp(args[0], "exit")) {
-    exit(0);
-  }
+	/*
+	strcmp() compares two strings
+	Returns 0 if they are equal which will make the if statement true
+	and thus
+	*/
+	if(!strcmp(args[0], "quit") || !strcmp(args[0], "exit")) {
+		exit(0);
+	}
 
-  /* check if async call */
-  if(!strcmp(args[nargs-1], "&")) { async = 1; args[--nargs] = 0; }
-  else async = 0;
+	/* check if async call */
+	if(!strcmp(args[nargs-1], "&")) { async = 1; args[--nargs] = 0; }
+	else async = 0;
 
-  pid = fork();
-  if(pid == 0) { /* child process */
-    execvp(args[0], args);
-    /* return only when exec fails */
-    perror("exec failed");
-    exit(-1);
-  } else if(pid > 0) { /* parent process */
-    if(!async) waitpid(pid, NULL, 0);
-    else printf("this is an async call\n");
-  } else { /* error occurred */
-    perror("fork failed");
-    exit(1);
-  }
+	pid = fork();
+	if(pid == 0) { /* child process */
+		execvp(args[0], args);
+		/* return only when exec fails */
+		perror("exec failed");
+		exit(-1);
+	} else if(pid > 0) { /* parent process */
+		if(!async) waitpid(pid, NULL, 0);
+		else printf("this is an async call\n");
+	} else { /* error occurred */
+		perror("fork failed");
+		exit(1);
+	}
 }
 
 int main (int argc, char* argv [])
 {
-  char cmdline[BUFSIZ];
+	char cmdline[BUFSIZ];
 
-  for(;;) {
-    printf("Cabrera-Cristian> ");
-    if(fgets(cmdline, BUFSIZ, stdin) == NULL) {
-      perror("fgets failed");
-      exit(1);
-    }
-    /*Reads the buffer or the input that was placed by the user*/
-    execute(cmdline) ;
-  }
-  return 0;
+	for(;;) {
+		printf("Cabrera-Cristian> ");
+		/* If users input on the commandline is longer then 1024 then it fails*/
+		if(fgets(cmdline, BUFSIZ, stdin) == NULL) {
+			perror("fgets failed");
+			exit(1);
+		}
+		/*Excutes the commandline instructions and passes an array with characters*/
+		execute(cmdline) ;
+	}
+	return 0;
 }
