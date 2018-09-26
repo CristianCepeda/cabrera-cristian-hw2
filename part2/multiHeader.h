65f1cdb04a687e19630add2116bbdbb3ac2de588
CristianCepeda <ccepe014@fiu.edu>
2018-09-26 19:17:34 -0400

    validated command line inputs

--- part2/multiHeader.h
+++ part2/multiHeader.h
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <pthread.h>
 
 // ENUM CONSTANTS
 enum CONSTANTS {FAIL = 0, PASS = 1, TOTAL_NUMBER_OF_ARGS = 2};
+enum ASCII{ZERO_ASCII = 48, NINE_ASCII = 57};
 
 int validateCmdLine(int argc, char* argv[]);
 int numberOfArguments(int argc);
-// void SimpleThread(int which);
+int checkForOnlyNumbers(int argc, char * argv[]);
+void SimpleThread(int which);
+
+// GLOBAL VARIABLES
+int SharedVariable; // Used in SimpleThread()
